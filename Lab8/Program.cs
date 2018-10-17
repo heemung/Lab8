58e99c4d7a54b8f5e4eb24af4bcc09600e3910ee
Clayton <clayton.cox@gmail.com>
2018-10-17 15:49:59 -0400

    all methods work

--- Lab8/Program.cs
+++ Lab8/Program.cs
@@ -26,14 +26,13 @@ namespace Lab8
             "fruit","coke"};
 
             int studentNum, orginalNum;
-            bool contYN = true, yesOrNo = true, validdata;
+            bool contYN = true, yesOrNo = true, validData = true;
             string userAnswer;
 
             Console.Write("Welcome to Our C# Class. ");
 
             while (contYN)
             {
-                validdata = true;
                 orginalNum = UserInput();
                 studentNum = orginalNum - 1;
                 try
@@ -43,47 +42,8 @@ namespace Lab8
                         names[studentNum], names[studentNum]);
 
                     userAnswer = Console.ReadLine().ToLower();
+                    Answers(validData, userAnswer, names, hometown, food, studentNum);
 
-                    while (validdata)
-                    {
-                        switch (userAnswer)
-                        {
-                            case "hometown":
-                                {
-                                    Console.WriteLine("{0} hometown is {1}. Would you like to know more?" +
-                                        " (enter 'yes' or 'no')", names[studentNum], hometown[studentNum]);
-
-                                    if (!ContinueMethod())
-                                    {
-                                        validdata = false;
-                                    }
-                                }
-                                break;
-
-                            case "favorite food":
-                                {
-                                    Console.WriteLine("{0} favorite food is {1}. Would you like to know more?" +
-                                        " (enter 'yes' or 'no')", names[studentNum], food[studentNum]);
-                                    if (!ContinueMethod())
-                                    {
-                                        validdata = false;
-                                    }
-                                }
-                                break;
-                            default:
-                                {
-                                    Console.WriteLine("That data does not exist. Please Try Again: \n");
-                                    break;
-                                }
-
-                        }
-                        if (validdata == true)
-                        {
-                            Console.WriteLine("What do you want to know about {0}? (enter of 'hometown' or " +
-                                "'favorite food')", names[studentNum]);
-                            userAnswer = Console.ReadLine().ToLower();
-                        }
-                    }
                 }
                 catch(IndexOutOfRangeException)
                 {
@@ -131,9 +91,49 @@ namespace Lab8
                return ContinueMethod();
             }
         }
-        static string CheckUserAnswe(string temp)
+        static void Answers(bool validData, string userAnswer,string[] names,
+            string[]hometown,string[] food, int studentNum )
         {
-         
+            while (validData)
+            {
+                switch (userAnswer)
+                {
+                    case "hometown":
+                        {
+                            Console.WriteLine("{0} hometown is {1}. Would you like to know more?" +
+                                " (enter 'yes' or 'no')", names[studentNum], hometown[studentNum]);
+
+                            if (!ContinueMethod())
+                            {
+                                validData = false;
+                            }
+                        }
+                        break;
+
+                    case "favorite food":
+                        {
+                            Console.WriteLine("{0} favorite food is {1}. Would you like to know more?" +
+                                " (enter 'yes' or 'no')", names[studentNum], food[studentNum]);
+                            if (!ContinueMethod())
+                            {
+                                validData = false;
+                            }
+                        }
+                        break;
+                    default:
+                        {
+                            Console.WriteLine("That data does not exist. Please Try Again: \n");
+                            break;
+                        }
+
+                }
+                if (validData == true)
+                {
+                    Console.WriteLine("What do you want to know about {0}? (enter of 'hometown' or " +
+                        "'favorite food')", names[studentNum]);
+                    userAnswer = Console.ReadLine().ToLower();
+                }
+            }
         }
 
     }
